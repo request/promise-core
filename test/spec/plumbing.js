d5f02f0078fa4e7e1ddc3984a3994198d3167bf2
analog-nico <nicolai.kamenzky@testrails.org>
2016-06-04 15:46:10 -0700

    test: expose functions

--- test/spec/plumbing.js
+++ test/spec/plumbing.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var plumbing = require('../../');
+var Bluebird = require('bluebird'),
+    plumbing = require('../../');
 
 
 describe('Promise-Core\'s Plumbing', function () {
@@ -30,6 +31,173 @@ describe('Promise-Core\'s Plumbing', function () {
                 PromiseImpl: function () {}
             });
         }).not.to.throw();
+
+    });
+
+    describe('should expose a method', function () {
+
+        var exposePromiseMethod = plumbing({ PromiseImpl: Bluebird }).exposePromiseMethod;
+
+        it('with default binding', function () {
+
+            var target = {
+                fakePromise: {
+                    attr: 5,
+                    then: function () {
+                        return this.attr;
+                    }
+                }
+            };
+
+            exposePromiseMethod(target, null, 'fakePromise', 'then');
+
+            expect(target.then()).to.eql(5);
+
+        });
+
+        it('with custom binding', function () {
+
+            var target = {};
+            var source = {
+                fakePromise: {
+                    attr: 5,
+                    then: function () {
+                        return this.attr;
+                    }
+                }
+            };
+
+            exposePromiseMethod(target, source, 'fakePromise', 'then');
+
+            expect(target.then()).to.eql(5);
+
+        });
+
+        it('with a different name', function () {
+
+            var target = {
+                fakePromise: {
+                    attr: 5,
+                    then: function () {
+                        return this.attr;
+                    }
+                }
+            };
+
+            exposePromiseMethod(target, null, 'fakePromise', 'then', 'next');
+
+            expect(target.next()).to.eql(5);
+
+        });
+
+        it('and not overwrite existing method', function () {
+
+            var target = {
+                fakePromise: {
+                    attr: 5,
+                    then: function () {
+                        return this.attr;
+                    }
+                }
+            };
+
+            expect(function () {
+                exposePromiseMethod(target, null, 'fakePromise', 'then', 'fakePromise');
+            }).to.throw('Unable to expose method "fakePromise"');
+
+        });
+
+        it('and forward arguments when called', function () {
+
+            var target = {
+                fakePromise: {
+                    attr: 5,
+                    then: function (a, b) {
+                        return this.attr + a + b;
+                    }
+                }
+            };
+
+            exposePromiseMethod(target, null, 'fakePromise', 'then');
+
+            expect(target.then(7, 11)).to.eql(5+7+11);
+
+        });
+
+    });
+
+    describe('should expose the promise', function () {
+
+        var exposePromise = plumbing({ PromiseImpl: Bluebird }).exposePromise;
+
+        it('with default binding', function () {
+
+            var target = {
+                fakePromise: {
+                    attr: 5,
+                    then: function () {
+                        return this.attr;
+                    }
+                }
+            };
+
+            exposePromise(target, null, 'fakePromise');
+
+            expect(target.promise().then()).to.eql(5);
+
+        });
+
+        it('with custom binding', function () {
+
+            var target = {};
+            var source = {
+                fakePromise: {
+                    attr: 5,
+                    then: function () {
+                        return this.attr;
+                    }
+                }
+            };
+
+            exposePromise(target, source, 'fakePromise');
+
+            expect(target.promise().then()).to.eql(5);
+
+        });
+
+        it('with a different name', function () {
+
+            var target = {
+                fakePromise: {
+                    attr: 5,
+                    then: function () {
+                        return this.attr;
+                    }
+                }
+            };
+
+            exposePromise(target, null, 'fakePromise', 'promise2');
+
+            expect(target.promise2().then()).to.eql(5);
+
+        });
+
+        it('and not overwrite existing method', function () {
+
+            var target = {
+                fakePromise: {
+                    attr: 5,
+                    then: function () {
+                        return this.attr;
+                    }
+                }
+            };
+
+            expect(function () {
+                exposePromise(target, null, 'fakePromise', 'fakePromise');
+            }).to.throw('Unable to expose method "fakePromise"');
+
+        });
 
     });
 
