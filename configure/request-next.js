bf3af628493f9ceb2049ea0c57839acfcafb502b
analog-nico <nicolai.kamenzky@testrails.org>
2016-06-04 14:21:46 -0700

    fix: options validation

--- configure/request-next.js
+++ configure/request-next.js
@@ -2,19 +2,35 @@
 
 var core = require('../'),
     isArray = require('lodash/isArray'),
-    isFunction = require('lodash/isFunction');
+    isFunction = require('lodash/isFunction'),
+    isObjectLike = require('lodash/isObjectLike');
 
 
 module.exports = function (options) {
 
-    var errorText = 'Please verify options.'; // For better minification because this string is repeating
+    var errorText = 'Please verify options'; // For better minification because this string is repeating
+
+    if (!isObjectLike(options)) {
+        throw new TypeError(errorText);
+    }
 
     if (!isFunction(options.client)) {
-        throw new TypeError(errorText + 'client');
+        throw new TypeError(errorText + '.client');
     }
 
     if (!isArray(options.expose) || options.expose.length === 0) {
-        throw new TypeError(errorText + 'expose');
+        throw new TypeError(errorText + '.expose');
+    }
+
+    var thenExposed = false;
+    for ( var i = 0; i < options.expose.length; i+=1 ) {
+        if (options.expose[i] === 'then') {
+            thenExposed = true;
+            break;
+        }
+    }
+    if (!thenExposed) {
+        throw new Error('Please expose "then"');
     }
 
 
@@ -30,9 +46,17 @@ module.exports = function (options) {
 
         var request = options.client(requestOptions);
 
-        for ( var i = 0; i < options.expose.length; i+=1 ) {
-            var method = options.expose[i];
-            plumbing[ method === 'promise' ? 'exposePromise' : 'exposePromiseMethod' ](request, self, '_rp_promise', method);
+        for ( var k = 0; k < options.expose.length; k+=1 ) {
+
+            var method = options.expose[k];
+
+            plumbing[ method === 'promise' ? 'exposePromise' : 'exposePromiseMethod' ](
+                request,
+                self,
+                '_rp_promise',
+                method
+            );
+
         }
 
         return request;
--- configure/request2.js
+++ configure/request2.js
@@ -2,19 +2,24 @@
 
 var core = require('../'),
     isArray = require('lodash/isArray'),
-    isFunction = require('lodash/isFunction');
+    isFunction = require('lodash/isFunction'),
+    isObjectLike = require('lodash/isObjectLike');
 
 
 module.exports = function (options) {
 
-    var errorText = 'Please verify options.'; // For better minification because this string is repeating
+    var errorText = 'Please verify options'; // For better minification because this string is repeating
+
+    if (!isObjectLike(options)) {
+        throw new TypeError(errorText);
+    }
 
     if (!isFunction(options.request)) {
-        throw new TypeError(errorText + 'request');
+        throw new TypeError(errorText + '.request');
     }
 
     if (!isArray(options.expose) || options.expose.length === 0) {
-        throw new TypeError(errorText + 'expose');
+        throw new TypeError(errorText + '.expose');
     }
 
 
@@ -24,10 +29,26 @@ module.exports = function (options) {
 
     plumbing.interceptInit(options.request);
 
-    var proto = options.request.Request.prototype;
+    var thenExposed = false;
     for ( var i = 0; i < options.expose.length; i+=1 ) {
+
         var method = options.expose[i];
-        plumbing[ method === 'promise' ? 'exposePromise' : 'exposePromiseMethod' ](proto, null, '_rp_promise', method);
+
+        plumbing[ method === 'promise' ? 'exposePromise' : 'exposePromiseMethod' ](
+            options.request.Request.prototype,
+            null,
+            '_rp_promise',
+            method
+        );
+
+        if (method === 'then') {
+            thenExposed = true;
+        }
+
+    }
+
+    if (!thenExposed) {
+        throw new Error('Please expose "then"');
     }
 
 };
