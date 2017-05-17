eb39956ab3abc7432448df22cf20feb3d49caa57
Gurpreet Atwal <git@gatwal.com>
2017-05-17 16:46:12 -0700

    add tests for branch conditions in buildMessage

---
+++ test/spec/errors.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var errors = require('../../errors');
+
+describe('StatusCodeError', function () {
+
+    it('should use http message string for errors', function () {
+
+        var e = new errors.StatusCodeError(301);
+        expect(e.message).to.eql('301 - Found');
+
+        e = new errors.StatusCodeError(400);
+        expect(e.message).to.eql('400 - Bad Request');
+
+        e = new errors.StatusCodeError(502);
+        expect(e.message).to.eql('502 - Bad Gateway');
+
+    });
+
+    it('should default to generic suffix for unknown status codes', function () {
+
+        var e = new errors.StatusCodeError(160);
+        expect(e.message).to.eql('160 - Informational');
+
+        e = new errors.StatusCodeError(260);
+        expect(e.message).to.eql('260 - OK');
+
+        e = new errors.StatusCodeError(360);
+        expect(e.message).to.eql('360 - Redirection');
+
+        e = new errors.StatusCodeError(440);
+        expect(e.message).to.eql('440 - Client Error');
+
+        e = new errors.StatusCodeError(550);
+        expect(e.message).to.eql('550 - Server Error');
+
+    });
+
+    it('should default to just status code if it is not in range [100, 599]', function () {
+
+        var e = new errors.StatusCodeError(99);
+        expect(e.message).to.eql('99');
+
+        e = new errors.StatusCodeError(600);
+        expect(e.message).to.eql('600');
+
+    });
+
+});
